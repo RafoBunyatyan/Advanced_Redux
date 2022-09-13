98976cbfacbd4584013d1d221051de9a394e562c
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-09-13 23:31:21 +0400

    add  changed treu & false

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,10 @@ export const fetchCartData = () => {
 
 		try {
 			const cartData = await fetchData()
-			dispatch(cartActions.replaceCart(cartData))
+			dispatch(cartActions.replaceCart({
+				items: cartData.items || [],
+				totalQuantity: cartData.totalQuantity,
+			}))
 		} catch (error) {
 			dispatch(
 				uiActions.showNotification({
@@ -46,7 +49,10 @@ export const sendCartData = (cart) => {
 			const response = await fetch('https://react-http-752e6-default-rtdb.firebaseio.com/cart.json',
 				{
 					method: 'PUT',
-					body: JSON.stringify(cart),
+					body: JSON.stringify({
+						items: cart.items,
+						totalQuantity: cart.totalQuantity,
+					}),
 				}
 			)
 
